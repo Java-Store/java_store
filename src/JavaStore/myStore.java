a21a495c9795b8e419baf6512ee14e48f1a6f66f
DelMonroe <delenor@gmail.com>
2017-05-15 14:16:10 -0500

    Added all methods needed for store

--- src/JavaStore/myStore.java
+++ src/JavaStore/myStore.java
@@ -1,7 +1,7 @@
 package JavaStore;
 
 import java.util.Scanner;
-
+import java.util.Date;
 /**
  * Created by DelMonroe on 5/15/17.
  */
@@ -21,8 +21,32 @@ public class myStore {
         System.out.println("3) Exit\n");
     }
 
-    public static String Sale(String items) {
+    public static String Sale(String item) {
+        String items, name;
+        System.out.println("Please Enter your name");
         System.out.println("What Item do you want?");
+
         return items;
+    }
+
+    public static float Price(Float price) {
+        float prices;
+        System.out.println("Please Enter the Price");
+        return price;
+    }
+
+    public static int getQuantity(Integer itemQuantity) {
+        int itemQuantities;
+        return itemQuantity;
+    }
+
+    public static void getdateTime () {
+        Date date = new Date();
+        System.out.println(date);
+
+    }
+
+    public static void exitMenu() {
+
     }
 }
